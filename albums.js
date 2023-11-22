08487c366883b5fc263fb65283c8cf69aa5b7306
Catalin Radoi <radoi.catalin88@gmail.com>
2023-11-22 23:03:34 +0100

    finish

--- albums.js
+++ albums.js
@@ -71,53 +71,48 @@ let albums = [
     tracks: [
       {
         tId: 1,
-        tName: "Don't Stop 'Til You Get Enough",
-        tDuration: 365,
+        tName: "Wanna be startin' somethin' ",
+        tDuration: 363,
       },
       {
         tId: 2,
-        tName: "Rock with You",
-        tDuration: 220,
+        tName: "Baby be mine",
+        tDuration: 260,
       },
       {
         tId: 3,
-        tName: "Workin' Day and Night",
-        tDuration: 314,
+        tName: "The girl is mine feat. Paul McCartney",
+        tDuration: 222,
       },
       {
         tId: 4,
-        tName: "Get on the Floor",
-        tDuration: 277,
+        tName: "Thriller",
+        tDuration: 357,
       },
       {
         tId: 5,
-        tName: "Off the Wall",
-        tDuration: 246,
+        tName: "Beat it",
+        tDuration: 258,
       },
       {
         tId: 6,
-        tName: "Girlfriend",
-        tDuration: 184,
+        tName: "Bille Jean",
+        tDuration: 294,
       },
       {
         tId: 7,
-        tName: "She's Out of My Life",
-        tDuration: 218,
+        tName: "Human nature",
+        tDuration: 246,
       },
       {
         tId: 8,
-        tName: "I Can't Help it",
-        tDuration: 269,
+        tName: "P.Y.T.(Pretty Young Thing)",
+        tDuration: 239,
       },
       {
         tId: 9,
-        tName: "I'ts the Falling in Love",
-        tDuration: 227,
-      },
-      {
-        tId: 10,
-        tName: "Burn This Disco Out",
-        tDuration: 221,
+        tName: "The lady in my life",
+        tDuration: 300,
       },
     ],
   },
@@ -132,53 +127,58 @@ let albums = [
     tracks: [
       {
         tId: 1,
-        tName: "Don't Stop 'Til You Get Enough",
-        tDuration: 365,
+        tName: "Bad",
+        tDuration: 247,
       },
       {
         tId: 2,
-        tName: "Rock with You",
-        tDuration: 220,
+        tName: "The way you make me feel",
+        tDuration: 298,
       },
       {
         tId: 3,
-        tName: "Workin' Day and Night",
-        tDuration: 314,
+        tName: "Speed demon",
+        tDuration: 242,
       },
       {
         tId: 4,
-        tName: "Get on the Floor",
-        tDuration: 277,
+        tName: "Liberian Girl",
+        tDuration: 232,
       },
       {
         tId: 5,
-        tName: "Off the Wall",
+        tName: "Just good friends",
         tDuration: 246,
       },
       {
         tId: 6,
-        tName: "Girlfriend",
-        tDuration: 184,
+        tName: "Another part of me",
+        tDuration: 234,
       },
       {
         tId: 7,
-        tName: "She's Out of My Life",
-        tDuration: 218,
+        tName: "Man in the mirror",
+        tDuration: 318,
       },
       {
         tId: 8,
-        tName: "I Can't Help it",
-        tDuration: 269,
+        tName: "I just can't stop loving you",
+        tDuration: 251,
       },
       {
         tId: 9,
-        tName: "I'ts the Falling in Love",
-        tDuration: 227,
+        tName: "Dirty Diana",
+        tDuration: 280,
       },
       {
         tId: 10,
-        tName: "Burn This Disco Out",
-        tDuration: 221,
+        tName: "Smooth criminal",
+        tDuration: 257,
+      },
+      {
+        tId: 11,
+        tName: "Leave me alone",
+        tDuration: 280,
       },
     ],
   },
@@ -193,53 +193,73 @@ let albums = [
     tracks: [
       {
         tId: 1,
-        tName: "Don't Stop 'Til You Get Enough",
-        tDuration: 365,
+        tName: "Jam",
+        tDuration: 338,
       },
       {
         tId: 2,
-        tName: "Rock with You",
-        tDuration: 220,
+        tName: "Why you wanna trip on me",
+        tDuration: 323,
       },
       {
         tId: 3,
-        tName: "Workin' Day and Night",
-        tDuration: 314,
+        tName: "In the closet",
+        tDuration: 390,
       },
       {
         tId: 4,
-        tName: "Get on the Floor",
-        tDuration: 277,
+        tName: "She drives me wild",
+        tDuration: 219,
       },
       {
         tId: 5,
-        tName: "Off the Wall",
-        tDuration: 246,
+        tName: "Remember the time",
+        tDuration: 239,
       },
       {
         tId: 6,
-        tName: "Girlfriend",
-        tDuration: 184,
+        tName: "Can't let her get away",
+        tDuration: 298,
       },
       {
         tId: 7,
-        tName: "She's Out of My Life",
-        tDuration: 218,
+        tName: "Heal the World",
+        tDuration: 384,
       },
       {
         tId: 8,
-        tName: "I Can't Help it",
-        tDuration: 269,
+        tName: "Black or White",
+        tDuration: 254,
       },
       {
         tId: 9,
-        tName: "I'ts the Falling in Love",
-        tDuration: 227,
+        tName: "Who is it",
+        tDuration: 393,
       },
       {
         tId: 10,
-        tName: "Burn This Disco Out",
-        tDuration: 221,
+        tName: "Give in to me",
+        tDuration: 328,
+      },
+      {
+        tId: 11,
+        tName: "Will you be there",
+        tDuration: 460,
+      },
+      {
+        tId: 12,
+        tName: "Keep the faith",
+        tDuration: 356,
+      },
+      {
+        tId: 13,
+        tName: "Gone too soon",
+        tDuration: 201,
+      },
+      {
+        tId: 14,
+        tName: "Dangerous",
+        tDuration: 417,
       },
     ],
   },
@@ -254,53 +274,78 @@ let albums = [
     tracks: [
       {
         tId: 1,
-        tName: "Don't Stop 'Til You Get Enough",
-        tDuration: 365,
+        tName: "Billie Jean",
+        tDuration: 294,
       },
       {
         tId: 2,
-        tName: "Rock with You",
-        tDuration: 220,
+        tName: "The way you make me feel",
+        tDuration: 297,
       },
       {
         tId: 3,
-        tName: "Workin' Day and Night",
-        tDuration: 314,
+        tName: "Black or White",
+        tDuration: 255,
       },
       {
         tId: 4,
-        tName: "Get on the Floor",
-        tDuration: 277,
+        tName: "Rock with you",
+        tDuration: 220,
       },
       {
         tId: 5,
-        tName: "Off the Wall",
-        tDuration: 246,
+        tName: "She's out of my life",
+        tDuration: 217,
       },
       {
         tId: 6,
-        tName: "Girlfriend",
-        tDuration: 184,
+        tName: "Bad",
+        tDuration: 247,
       },
       {
         tId: 7,
-        tName: "She's Out of My Life",
-        tDuration: 218,
+        tName: "I just can't stop loving you",
+        tDuration: 251,
       },
       {
         tId: 8,
-        tName: "I Can't Help it",
-        tDuration: 269,
+        tName: "Man in the mirror",
+        tDuration: 319,
       },
       {
         tId: 9,
-        tName: "I'ts the Falling in Love",
-        tDuration: 227,
+        tName: "Thriller",
+        tDuration: 357,
       },
       {
         tId: 10,
-        tName: "Burn This Disco Out",
+        tName: "Beat it",
+        tDuration: 258,
+      },
+      {
+        tId: 11,
+        tName: "The girl is mine",
         tDuration: 221,
+      },
+      {
+        tId: 12,
+        tName: "Remember the time",
+        tDuration: 239,
+      },
+      {
+        tId: 13,
+        tName: "Don't stop 'Til you get enough",
+        tDuration: 364,
+      },
+      {
+        tId: 14,
+        tName: "Wanna be startin' somethin'",
+        tDuration: 362,
+      },
+      {
+        tId: 15,
+        tName: "Heal the world",
+        tDuration: 385,
       },
     ],
   },
@@ -315,53 +360,78 @@ let albums = [
     tracks: [
       {
         tId: 1,
-        tName: "Don't Stop 'Til You Get Enough",
-        tDuration: 365,
+        tName: "Billie Jean",
+        tDuration: 294,
       },
       {
         tId: 2,
-        tName: "Rock with You",
-        tDuration: 220,
+        tName: "The way you make me feel",
+        tDuration: 297,
       },
       {
         tId: 3,
-        tName: "Workin' Day and Night",
-        tDuration: 314,
+        tName: "Black or White",
+        tDuration: 255,
       },
       {
         tId: 4,
-        tName: "Get on the Floor",
-        tDuration: 277,
+        tName: "Rock with you",
+        tDuration: 220,
       },
       {
         tId: 5,
-        tName: "Off the Wall",
-        tDuration: 246,
+        tName: "She's out of my life",
+        tDuration: 217,
       },
       {
         tId: 6,
-        tName: "Girlfriend",
-        tDuration: 184,
+        tName: "Bad",
+        tDuration: 247,
       },
       {
         tId: 7,
-        tName: "She's Out of My Life",
-        tDuration: 218,
+        tName: "I just can't stop loving you",
+        tDuration: 251,
       },
       {
         tId: 8,
-        tName: "I Can't Help it",
-        tDuration: 269,
+        tName: "Man in the mirror",
+        tDuration: 319,
       },
       {
         tId: 9,
-        tName: "I'ts the Falling in Love",
-        tDuration: 227,
+        tName: "Thriller",
+        tDuration: 357,
       },
       {
         tId: 10,
-        tName: "Burn This Disco Out",
+        tName: "Beat it",
+        tDuration: 258,
+      },
+      {
+        tId: 11,
+        tName: "The girl is mine",
         tDuration: 221,
+      },
+      {
+        tId: 12,
+        tName: "Remember the time",
+        tDuration: 239,
+      },
+      {
+        tId: 13,
+        tName: "Don't stop 'Til you get enough",
+        tDuration: 364,
+      },
+      {
+        tId: 14,
+        tName: "Wanna be startin' somethin'",
+        tDuration: 362,
+      },
+      {
+        tId: 15,
+        tName: "Heal the world",
+        tDuration: 385,
       },
     ],
   },
@@ -445,11 +515,12 @@ let albums = [
 // const duration = document.querySelector("#durata");
 const rowJS = document.querySelector("#js");
 
-function generaAlbum() {
+//è chiamata in modo errato, qui in realtà generlo si gli album ma sottoforma di card
+function generaCards() {
   for (let i = 0; i < albums.length; i++) {
-    let ore = Math.floor(albums[i].duration / 3600);
-    let minuti = Math.floor((albums[i].duration % 3600) / 60);
-    let secondi = albums[i].duration % 60;
+    let oreAlbum = Math.floor(albums[i].duration / 3600);
+    let minutiAlbum = Math.floor((albums[i].duration % 3600) / 60);
+    let secondiAlbum = albums[i].duration % 60;
     rowJS.innerHTML += ` <div class="col-xl-3 col-md-4 col-6">
         <div class="card card-effect">
           <img class="img-thumbnail" src="${albums[i].cover}">
@@ -458,7 +529,7 @@ function generaAlbum() {
             <p>Artista: <span id="artist">${albums[i].artist}</span></p>
             <p>Genere: <span id="genre">${albums[i].genre}</span></p>
             <p>Anno: <span id="genre">${albums[i].releaseYear}</span></p>
-            <p>Durata: <span id="duration">${ore}h, ${minuti}minuti e ${secondi}secondi</span></p>
+            <p>Durata: <span id="duration"> ${oreAlbum}h, ${minutiAlbum}minuti e ${secondiAlbum}secondi</span></p>
             <a href="#focus" class="btn btn-primary">Dettagli...</a>
           </div>
         </div>
@@ -466,17 +537,21 @@ function generaAlbum() {
   }
 }
 
-generaAlbum();
+generaCards();
 
-// function creaDettaglioAlbum() {
-//   focus.arguments;
-// }
 const button = document.querySelectorAll(".btn-primary");
 const focus = document.querySelector("#dettagli-album");
 
+//nel caso in cui viene cliccato il tasto "dettagli" mi chiamo la funzoone sotto dove ciclo l'array x tirare fuori tutte l info
+//ci sarà uin for annidato perchè le canzoni dell'album sono salvate sotto forma di array di oggetti (dentr quindi all'oggetto album, a sua volta dentro un array)
 for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", function () {
-    //console.log("si strunz");
+  //variabili che servono per convertire i secondi della traccia in h,m,s
+  let oreAlbum = Math.floor(albums[i].duration / 3600);
+  let minutiAlbum = Math.floor((albums[i].duration % 3600) / 60);
+  let secondiAlbum = albums[i].duration % 60;
+
+  function generaAlbum() {
+    //cancello la sezione "Nessun album selezionato [...]"
     if (focus) {
       focus.remove();
     }
@@ -489,18 +564,33 @@ for (let i = 0; i < button.length; i++) {
             </div>
             <div class="tracks">
               <ul id="lista"></ul>`;
-    for (let j = 0; j <= albums[i].tracks.length; j++) {
-      if (albums[i] && albums[i].tracks[j].tName) {
-        document.querySelector("#lista").innerHTML += `<li>${j + 1} - ${albums[i].tracks[j].tName}</li>`;
+
+    //estraggo i brani (tracks) dall'array albums
+    for (let j = 0; j < albums[i].tracks.length; j++) {
+      //calcolo minuti e secondi dei brani
+      let tMinuti = Math.floor((albums[i].tracks[j].tDuration % 3600) / 60);
+      let tSecondi = albums[i].tracks[j].tDuration % 60;
+
+      //x evitare eventuale errore ncaught TypeError: Cannot read properties of undefined (reading 'tName')
+      if (albums[i].tracks[j].tName) {
+        document.querySelector("#lista").innerHTML += `<li>${j + 1} - ${albums[i].tracks[j].tName} - ${tMinuti}:${tSecondi}</li>`;
       }
     }
-    console.log("AAA");
-    document.querySelector(".album-details").innerHTML += ` 
+    let up = document.createElement("div");
+    up.classList.add("align-self-end");
+    up.innerHTML = `<a href="#nav"><input type="button" class="btn btn-danger" value="Up" /></a>`;
+
+    //completo l'album con i dettagli
+    document.querySelector(".album-details").innerHTML = ` 
             <ul>
-              <li>${albums[i].artist}</li>
-              <li>${albums[i].genre}</li>
-              <li>${albums[i].title}</li>
+              <li>Artista: <span>${albums[i].artist}</span></li>
+              <li>Genere: <span>${albums[i].genre}</span></li>
+              <li>Titolo dell'album: <span>${albums[i].title}</span></li>
+              <li>Durata totale: <span>${oreAlbum}h, ${minutiAlbum}minuti e ${secondiAlbum}secondi</span></li>
             </ul>
           </div>`;
-  });
+    document.querySelector(".album-details").appendChild(up);
+  }
+
+  button[i].addEventListener("click", generaAlbum);
 }
